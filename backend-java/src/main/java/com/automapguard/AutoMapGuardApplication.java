f0ef5d980dbf501d0a49257b9e0addccc1d847e7
ishan22399 <ishanshivankar14@gmail.com>
2026-01-15 22:01:29 +0530

    feat: add Spring Boot application entry point

---
+++ backend-java/src/main/java/com/automapguard/AutoMapGuardApplication.java
@@ -0,0 +1,13 @@
+package com.automapguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMapGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMapGuardApplication.class, args);
+    }
+
+}
